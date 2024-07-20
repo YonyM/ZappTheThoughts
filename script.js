e78dccd232677b3d6b444498b16de30e8e90f566
YonyM <80950518+YonyM@users.noreply.github.com>
2024-07-20 16:01:53 +0200

    technisches update

--- script.js
+++ script.js
@@ -1,46 +1,30 @@
 
-/*const burgermenu = document.getElementById('burgermenu');
-const menu = document.getElementById('menu');
-
-let open = false;
-
-
-if (burgermenu && menu) {
-
-    burgermenu.addEventListener('click', () => {
-        open = !open
-        if (open) {
-            menu.style.display = 'flex'
-        }
-
-        else {
-            menu.style.display = 'none'
-        }
-    });
-
-}*/
 
 const burgerMenu = document.querySelector('.burgermenu');
 const menu = document.querySelector('.menu');
 
-const poppup = document.querySelector('.poppup')
-const contentline = document.querySelector('.content-line')
+const poppups = document.querySelectorAll('.poppup');
+const contentLines = document.querySelectorAll('.content-line');
 
-poppup.addEventListener('mouseenter', function() {
-    contentline.style.width = '200px';
-})
-poppup.addEventListener('mouseleave', function() {
-    contentline.style.width = '20px';
-})
+poppups.forEach((poppup, i) => {
+  const content = poppup.querySelector('.content');
+  const contentLine = contentLines[i];
 
+  content.addEventListener('mouseenter', () => {
+    contentLine.style.width = '200px';
+  });
+
+  content.addEventListener('mouseleave', () => {
+    contentLine.style.width = '30px';
+  });
+});
 
 burgerMenu.addEventListener('click', function() {
+  if (!menu.classList.contains('open')) {
+    menu.classList.add('open');
+  } else {
+    menu.classList.remove('open');
+  }
+});
 
-    if (!menu.classList.contains('open')) {
-        menu.classList.add('open');
-    }
-    else {
-        menu.classList.remove('open')
-    }
-    
-})
+
